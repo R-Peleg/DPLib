edcabcb4257e3aafef4f7a15caf80e6385358a61
Reuven Peleg <4018286+R-Peleg@users.noreply.github.com>
2026-01-06 08:19:28 +0200

    Full working proof for sensitivity of count

--- DPLib/Queries.lean
+++ DPLib/Queries.lean
@@ -12,6 +12,13 @@ def countMatching (criteria : α -> Bool) : Query ι α ℕ :=
 def countEntries : Query ι α ℕ :=
   countMatching (fun _ => true)
 
+-- TODO: Import from mathlib once we upgrade our version
+lemma card_sub_card_eq (s t : Finset α) : t.card - s.card = (t \ s).card - (s \ t).card :=
+  calc
+    t.card - s.card = t.card - (s ∩ t).card - (s \ t).card := by grind
+    _ = (t \ (s ∩ t)).card - (s \ t).card := by grind
+    _ = (t \ s).card - (s \ t).card := by grind
+
 theorem count_sensitivity_one (criteria : α → Bool) :
     has_sensitivity (countMatching criteria : Query ι α ℕ) 1 := by
   rw [has_sensitivity]
@@ -22,7 +29,36 @@ theorem count_sensitivity_one (criteria : α → Bool) :
 
   let S1 := (Finset.univ.filter (fun i => criteria (db1 i)))
   let S2 := (Finset.univ.filter (fun i => criteria (db2 i)))
+  have h_diff_card : S1 \ S2 ⊆ {k} ∧ S2 \ S1 ⊆ {k} := by grind
+  have h_card_diff1 : S1.card - S2.card <= 1 := by
+    rw [card_sub_card_eq]
+    by_cases k ∈ S1 \ S2
+    · have h1: (S1 \ S2) == {k} := by grind
+      grind
+    · have h1: (S1 \ S2) == {} := by grind
+      grind
+  have h_card_diff1_R : (S1.card : ℝ) - (S2.card : ℝ) <= 1 := by
+    simp
+    simp at h_card_diff1
+    norm_cast
+  have h_card_diff2 : S2.card - S1.card <= 1 := by
+    rw [card_sub_card_eq]
+    by_cases k ∈ S2 \ S1
+    · have h1: (S2 \ S1) == {k} := by grind
+      grind
+    · have h1: (S2 \ S1) == {} := by grind
+      grind
+  have h_card_diff2_R : (S2.card : ℝ) - (S1.card : ℝ) <= 1 := by
+    simp
+    simp at h_card_diff2
+    norm_cast
 
-  calc |(S1.card : ℝ) - (S2.card: ℝ)|
-
-  _ <= 1 := by sorry
+  have h_card_abs_diff : |(S1.card : ℝ) - (S2.card: ℝ)| <= 1 := by
+    by_cases h_pos : 0 ≤ (S1.card : ℝ) - (S2.card : ℝ)
+    · rw [abs_eq_self.mpr h_pos]
+      linarith
+    · push_neg at h_pos
+      apply le_of_lt at h_pos
+      rw [abs_eq_neg_self.mpr h_pos]
+      linarith
+  grind
